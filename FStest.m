25d6cddba8a3d6965b3cee7dc599aebe499522bd
NicholasPrice <nicholas.price@monash.edu>
2022-08-06 12:59:13 +1000

    introduce tPreBlank parameter

---
+++ FStest.m
@@ -0,0 +1,5 @@
+nystagmus_gabor('test','phaseSpeed',10, 'orientation',[0 180], 'sigma',2, ...
+    'frequency',.5, 'tPreBlank', [2000 0]);
+
+
+nystagmus_gabor('test','phaseSpeed',10, 'orientation',[0 180], 'sigma',2, 'frequency',.5);
